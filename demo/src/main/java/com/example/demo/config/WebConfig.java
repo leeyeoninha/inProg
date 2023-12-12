d3dc2b0529ba7e3536bd9296b3465684005b0226
Chung10Kr <coohg93@hanmail.net>
2023-12-12 10:14:09 +0900

    Project 초기 셋팅

---
+++ demo/src/main/java/com/example/demo/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // Vue.js 서버 주소
+                .allowedMethods("*") // 허용할 HTTP 메소드 (GET, POST, PUT, DELETE 등)
+                .allowedHeaders("*") // 허용할 헤더
+                .allowCredentials(true)
+                .maxAge(3600); // 사전 전달 요청(Preflight request) 캐시 시간
+    }
+}
---
+++ demo/src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    
+    // Test : 브라우저에 http://localhost:8080/hello?name=crlee         
+    @GetMapping("/hello")
+    public String sayHellow(String name){
+        return "Hello "+name;
+    }
+}

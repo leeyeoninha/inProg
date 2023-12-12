d199d548d06a099d843b87765ec7c354a6888665
Chung10Kr <coohg93@hanmail.net>
2023-12-12 21:55:43 +0900

    Get Parameter @RequestParam 명시

--- demo/src/main/java/com/example/demo/controller/MainController.java
+++ demo/src/main/java/com/example/demo/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,7 +10,7 @@ public class MainController {
     
     // Test : 브라우저에 http://localhost:8080/hello?name=crlee         
     @GetMapping("/hello")
-    public String sayHellow(String name){
+    public String sayHellow(@RequestParam("name") String name){
         return "Hello "+name;
     }
 }

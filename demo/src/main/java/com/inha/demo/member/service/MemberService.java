e5b2892b76d58b0258fddca5ec5a022120db9759
Chung10Kr <coohg93@hanmail.net>
2023-12-17 02:04:06 +0900

    remove interface

--- demo/src/main/java/com/inha/demo/member/service/MemberService.java
+++ demo/src/main/java/com/inha/demo/member/service/MemberService.java
@@ -2,8 +2,23 @@ package com.inha.demo.member.service;
 
 import java.util.NoSuchElementException;
 
+import org.springframework.stereotype.Service;
+
+import com.inha.demo.member.dao.MemberMapper;
 import com.inha.demo.member.dto.Member;
 
-public interface MemberService {
-    public Member getMember(String name) throws NoSuchElementException;
+@Service
+public class MemberService{
+
+    private MemberMapper memberMapper;
+
+    public Member getMember(String name) throws NoSuchElementException{
+        Member member = memberMapper.getMemberByName(name);
+
+        if( member == null ){
+            throw new NoSuchElementException("Not Found Member");
+        }
+        return member;
+    }
+    
 }

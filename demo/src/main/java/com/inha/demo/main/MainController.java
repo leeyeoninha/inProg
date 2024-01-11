629a221596c14c8bd24ba85182788fecf671cfb8
Chung10Kr <coohg93@hanmail.net>
2024-01-12 00:53:20 +0900

    리턴 문자열 변경

--- demo/src/main/java/com/inha/demo/main/MainController.java
+++ demo/src/main/java/com/inha/demo/main/MainController.java
@@ -23,7 +23,7 @@ public class MainController {
         String result;
         try{
             member = memberService.getMember(name);
-            result = "Hellow! My Name is " + member.getName() + " " + member.getAge()+" years old";
+            result = "Hellow! 저는 " + member.getName() + "입니다. " + member.getAge()+"살 입니다.";
         }catch(NoSuchElementException e){
             result = "Member Data가 없습니다.";
         }
--- demo/src/main/java/com/inha/demo/member/dao/MemberMapper.java
+++ demo/src/main/java/com/inha/demo/member/dao/MemberMapper.java
@@ -1,12 +1,9 @@
 package com.inha.demo.member.dao;
-
-
-import com.inha.demo.member.dto.Member;
 import org.apache.ibatis.annotations.Mapper;
 
+import com.inha.demo.member.dto.Member;
 
 @Mapper
 public interface MemberMapper {
     public Member getMemberByName(String name);
-    
 }

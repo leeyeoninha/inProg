0de430c51213505c609b8d2c3d521705b94c46ee
Chung10Kr <coohg93@hanmail.net>
2024-01-13 00:36:57 +0900

    주석 추가

--- demo/src/main/java/com/inha/demo/member/service/MemberService.java
+++ demo/src/main/java/com/inha/demo/member/service/MemberService.java
@@ -8,16 +8,23 @@ import org.springframework.stereotype.Service;
 import com.inha.demo.member.dao.MemberMapper;
 import com.inha.demo.member.dto.Member;
 
-@Service
+@Service // 비즈니스 로직을 처리(Service)를 처리하기 위한 클래스에 설정합니다.
 public class MemberService{
 
-    @Autowired
+    @Autowired //MemberMapper 인스턴스를 주입받기 위해 사용됩니다.
     private MemberMapper memberMapper;
 
+    /**
+     * 주어진 이름으로 회원 정보를 조회합니다.
+     * @param name 회원 이름
+     * @return 조회된 회원 정보
+     * @throws NoSuchElementException 회원 정보가 없을 경우 예외 발생
+     */
     public Member getMember(String name) throws NoSuchElementException{
         Member member = memberMapper.getMemberByName(name);
 
         if( member == null ){
+            // 조회된 member가 없을 경우 NoSuchElementException 예외를 던집니다.
             throw new NoSuchElementException("Not Found Member");
         }
         return member;
--- my-app/src/components/HelloWorld.vue
+++ my-app/src/components/HelloWorld.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="hello">
+    <!-- 사용자 이름을 입력받는 input 요소 -->
     <input v-model="name" type="text" placeholder="Input Your Name">
+    <!-- fetchHello 메소드를 실행하는 버튼 -->
     <button v-on:click="fetchHello">Send</button>
-    <h1>{{ msg }}</h1>
+    <!-- 응답 메시지를 표시하는 h1 요소 -->
   </div>
 </template>
 
@@ -15,6 +17,7 @@ export default {
   name: 'HelloWorld',
   data() {
     return {
+      // 사용자 이름과 응답 메시지를 저장하는 변수
       name : null,
       msg: null,
     };
@@ -22,8 +25,10 @@ export default {
   
   methods: {
     fetchHello() {
+      // 서버로 부터 데이터를 가져오는 메서드
       axios.get(`http://localhost:8080/hello?name=${this.name}`)
         .then(response => {
+          // 응답 데이터를 msg 변수에 저장
           this.msg = response.data;
         })
         .catch(error => {
@@ -34,7 +39,7 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- "scoped" 속성을 추가하여 CSS가 이 컴포넌트에만 적용되도록 함 -->
 <style scoped>
 h3 {
   margin: 40px 0 0;

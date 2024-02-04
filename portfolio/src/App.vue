a6bd6c8c3a14022a858f40f1f0970e502b3ad885
Chung10Kr <coohg93@hanmail.net>
2024-02-05 00:27:24 +0900

    -홍길동 로고 이미지 생성

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <img id="logo" alt="Vue logo" src="./assets/hong.jpg">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
@@ -15,6 +15,9 @@ export default {
 </script>
 
 <style>
+#logo {
+  border-radius: 10px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
